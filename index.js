24b0789184b435f727a04d1a8f29334c58eaf788
joss243 <mukanamj.devpro@gmail.com>
2020-04-23 20:47:56 +0200

    navigation responsive ok + initialisation materialize

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {
+    edge: 'left',
+    draggable: true,
+    inDuration: 250,
+    outDuration: 200,
+    onOpenStart: null,
+    onOpenEnd: null,
+    onCloseStart: null,
+    onCloseEnd: null,
+    preventScrolling: true
+});
+});
