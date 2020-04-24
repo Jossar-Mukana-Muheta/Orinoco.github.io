cfc4670c99e5ca740c9275216454de2a57d3c7fe
joss243 <mukanamj.devpro@gmail.com>
2020-04-24 21:47:36 +0200

    page accueil responsive + mise en place requette produits ok

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav')
   var instances = M.Sidenav.init(elems, {
     edge: 'left',
     draggable: true,
@@ -10,5 +10,27 @@ document.addEventListener('DOMContentLoaded', function() {
     onCloseStart: null,
     onCloseEnd: null,
     preventScrolling: true
-});
-});
+  })
+})
+
+let list = document.getElementById('list_produits')
+
+
+get_produit = () => {
+  let request = new XMLHttpRequest()
+  request.onreadystatechange = function () {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+      let response = JSON.parse(this.responseText)
+      for (let i = 0; i < response.length; i++) {
+        let newElt = document.createElement('li')
+        list.appendChild(newElt).innerHTML = (response[i].name)
+        console.log(i)
+      }
+    }
+    console.log('Lancer le serveur node');
+  }
+  request.open('GET', 'http://localhost:3000/api/cameras')
+  request.send()
+}
+
+window.onload = get_produit
