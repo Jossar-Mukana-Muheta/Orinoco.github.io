0d3109924cbf02eb23ebf861c1c467ed217b8095
joss243 <mukanamj.devpro@gmail.com>
2020-04-25 13:36:13 +0200

    page produit + requette image

--- index.js
+++ index.js
@@ -12,25 +12,50 @@ document.addEventListener('DOMContentLoaded', function () {
     preventScrolling: true
   })
 })
-
-let list = document.getElementById('list_produits')
-
-
-get_produit = () => {
-  let request = new XMLHttpRequest()
-  request.onreadystatechange = function () {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-      let response = JSON.parse(this.responseText)
-      for (let i = 0; i < response.length; i++) {
-        let newElt = document.createElement('li')
-        list.appendChild(newElt).innerHTML = (response[i].name)
-        console.log(i)
+window.addEventListener('load', (event) => {
+  /* Récuperer la liste des nom de produits */
+  getProduit = () => {
+    let list = document.getElementById('list_produits')
+    let request = new XMLHttpRequest()
+    request.onreadystatechange = function () {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        let response = JSON.parse(this.responseText)
+        for (let i = 0; i < response.length; i++) {
+          let newElt = document.createElement('li')
+          let newLink = document.createElement('a')
+          list.appendChild(newLink).setAttribute('href', 'produits.html')
+          newLink.appendChild(newElt).innerHTML = (response[i].name)
+        }
       }
+      console.log('Lancer le serveur node')
     }
-    console.log('Lancer le serveur node');
+    request.open('GET', 'http://localhost:3000/api/cameras')
+    request.send()
   }
-  request.open('GET', 'http://localhost:3000/api/cameras')
-  request.send()
-}
+  getProduit()
 
-window.onload = get_produit
+
+
+
+  /* Récuperer image des produit produits */
+  getProduitImage = () => {
+
+    let image = document.getElementById('produits_page__option')
+    let request = new XMLHttpRequest()
+    request.onreadystatechange = function () {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        let response = JSON.parse(this.responseText)
+        for (let i = 0; i < response.length; i++) {
+          let newSource = document.createElement('img')
+          image.appendChild(newSource).setAttribute('src', response[i].imageUrl)
+          console.log(response[i].imageUrl)
+        }
+      }
+      console.log('Lancer le serveur node')
+    }
+    request.open('GET', 'http://localhost:3000/api/cameras')
+    request.send()
+  }
+
+  getProduitImage()
+})
