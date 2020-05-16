c876d8ed7d6886aece00de3ce2ab6a6aa076ff87
Jossar Mukana Muheta <muhetajossar@gmail.com>
2020-05-16 18:11:50 +0200

    correction error console panierJSON (null)

--- panier.js
+++ panier.js
@@ -26,113 +26,118 @@ function getProduitID() {
   let PanierContenu = document.getElementById("panierContenu");
   let totalite;
 
-  for (let i = 0; i < panierJson.length; i++) {
-    const getProduitIDs = new RequetebddId();
-    getProduitIDs.getInformationId(panierJson[i].produit)
-      .then((responseText) => {
-        containerPanier.style.display = "block";
-        defautPage.style.display = "none";
+  if (panierJson) {
 
-        let panierProduit = document.createElement("div");
-        PanierContenu.appendChild(panierProduit);
-        panierProduit.setAttribute("class", "panierProduit");
+    for (let i = 0; i < panierJson.length; i++) {
+      const getProduitIDs = new RequetebddId();
+      getProduitIDs.getInformationId(panierJson[i].produit)
+        .then((responseText) => {
+          containerPanier.style.display = "block";
+          defautPage.style.display = "none";
 
-        let containerImage = document.createElement("div");
-        panierProduit.appendChild(containerImage);
-        containerImage.setAttribute("class", "containerImage");
+          let panierProduit = document.createElement("div");
+          PanierContenu.appendChild(panierProduit);
+          panierProduit.setAttribute("class", "panierProduit");
 
-        let panierImage = document.createElement("img");
-        containerImage.appendChild(panierImage);
-        panierImage.setAttribute("src", responseText.imageUrl);
-        panierImage.setAttribute("alt", "appareil photo")
+          let containerImage = document.createElement("div");
+          panierProduit.appendChild(containerImage);
+          containerImage.setAttribute("class", "containerImage");
 
-        let panierResumeDiv = document.createElement("div");
-        panierProduit.appendChild(panierResumeDiv);
-        panierResumeDiv.setAttribute("class", "ResumeDiv");
+          let panierImage = document.createElement("img");
+          containerImage.appendChild(panierImage);
+          panierImage.setAttribute("src", responseText.imageUrl);
+          panierImage.setAttribute("alt", "appareil photo")
 
-        let resumeTitreLink = document.createElement("a")
-        panierResumeDiv.appendChild(resumeTitreLink)
-        resumeTitreLink.setAttribute("href",
-          "produits.html?id=" + panierJson[i].produit)
+          let panierResumeDiv = document.createElement("div");
+          panierProduit.appendChild(panierResumeDiv);
+          panierResumeDiv.setAttribute("class", "ResumeDiv");
 
-        let resumeTitre = document.createElement("p");
-        resumeTitreLink.appendChild(resumeTitre);
-        resumeTitre.setAttribute("class", "resumeTitre");
-        resumeTitre.innerHTML = responseText.name;
+          let resumeTitreLink = document.createElement("a")
+          panierResumeDiv.appendChild(resumeTitreLink)
+          resumeTitreLink.setAttribute("href",
+            "produits.html?id=" + panierJson[i].produit)
 
-
-
-        let resumeText = document.createElement("p");
-        panierResumeDiv.appendChild(resumeText);
-        resumeText.setAttribute("class", "resumeText");
-        resumeText.innerHTML = responseText.description;
-
-        let produitOption = document.createElement("p");
-        panierResumeDiv.appendChild(produitOption);
-        produitOption.setAttribute("class", "produitOption");
-        produitOption.innerHTML = "Type de lentille choisit : " + panierJson[i].option;
-
-        let produitQuantite = document.createElement("p");
-        panierResumeDiv.appendChild(produitQuantite);
-        produitQuantite.setAttribute("class", "produitQuantite");
-        produitQuantite.innerHTML = "Qté : " + panierJson[i].quantite;
-
-        let panierPrixDiv = document.createElement("div");
-        panierProduit.appendChild(panierPrixDiv);
-        panierPrixDiv.setAttribute("class", "prix");
-
-        let panierPrix = document.createElement("p");
-        panierPrixDiv.appendChild(panierPrix);
-        panierPrix.setAttribute("class", "prixPanier");
-        panierPrix.innerHTML = responseText.price + " €";
-
-
-        //Calcul du prix total 
-        let totalPrice = panierJson[i].quantite * responseText.price;
-        total.push(totalPrice)
-        totalite = total.reduce(myFunc)
-
-        function myFunc(total, num) {
-          return total + num;
-        }
-        let totalPanier = document.getElementById('prixTotal')
-        totalPanier.innerHTML = totalite + " €"
-
-
-        //calcul de la quantité d'article
-        quantiteProduit = parseInt(panierJson[i].quantite)
-
-        quantiteTableau.push(quantiteProduit)
-        quantiteAddition = quantiteTableau.reduce(myFunc2)
-
-        function myFunc2(total, num) {
-          return parseInt(total + num)
-        }
-
-        let quantitéTotal = document.getElementById('quantitéTotal')
-        quantitéTotal.innerHTML = quantiteAddition
-
-        let pluriel = document.getElementById('pluriel')
-        if (quantiteAddition > 1) {
-          pluriel.innerHTML = "articles"
-        } else {
-          pluriel.innerHTML = "article"
-        }
+          let resumeTitre = document.createElement("p");
+          resumeTitreLink.appendChild(resumeTitre);
+          resumeTitre.setAttribute("class", "resumeTitre");
+          resumeTitre.innerHTML = responseText.name;
 
 
 
+          let resumeText = document.createElement("p");
+          panierResumeDiv.appendChild(resumeText);
+          resumeText.setAttribute("class", "resumeText");
+          resumeText.innerHTML = responseText.description;
 
-      })
-      .catch((error) => {
-        let panierVideText = document.getElementById('panierVideText')
-        panierVideText.innerHTML = "Erreur de chargement du panier"
+          let produitOption = document.createElement("p");
+          panierResumeDiv.appendChild(produitOption);
+          produitOption.setAttribute("class", "produitOption");
+          produitOption.innerHTML = "Type de lentille choisit : " + panierJson[i].option;
 
-        let lienPanierVide = document.getElementById('lienPanierVide')
-        lienPanierVide.innerHTML = "Recharger la page"
-        lienPanierVide.setAttribute('href',
-          'panier.html')
-      });
+          let produitQuantite = document.createElement("p");
+          panierResumeDiv.appendChild(produitQuantite);
+          produitQuantite.setAttribute("class", "produitQuantite");
+          produitQuantite.innerHTML = "Qté : " + panierJson[i].quantite;
+
+          let panierPrixDiv = document.createElement("div");
+          panierProduit.appendChild(panierPrixDiv);
+          panierPrixDiv.setAttribute("class", "prix");
+
+          let panierPrix = document.createElement("p");
+          panierPrixDiv.appendChild(panierPrix);
+          panierPrix.setAttribute("class", "prixPanier");
+          panierPrix.innerHTML = responseText.price + " €";
+
+
+          //Calcul du prix total 
+          let totalPrice = panierJson[i].quantite * responseText.price;
+          total.push(totalPrice)
+          totalite = total.reduce(myFunc)
+
+          function myFunc(total, num) {
+            return total + num;
+          }
+          let totalPanier = document.getElementById('prixTotal')
+          totalPanier.innerHTML = totalite + " €"
+
+
+          //calcul de la quantité d'article
+          quantiteProduit = parseInt(panierJson[i].quantite)
+
+          quantiteTableau.push(quantiteProduit)
+          quantiteAddition = quantiteTableau.reduce(myFunc2)
+
+          function myFunc2(total, num) {
+            return parseInt(total + num)
+          }
+
+          let quantitéTotal = document.getElementById('quantitéTotal')
+          quantitéTotal.innerHTML = quantiteAddition
+
+          let pluriel = document.getElementById('pluriel')
+          if (quantiteAddition > 1) {
+            pluriel.innerHTML = "articles"
+          } else {
+            pluriel.innerHTML = "article"
+          }
+
+
+
+
+        })
+        .catch((error) => {
+          let panierVideText = document.getElementById('panierVideText')
+          panierVideText.innerHTML = "Erreur de chargement du panier"
+
+          let lienPanierVide = document.getElementById('lienPanierVide')
+          lienPanierVide.innerHTML = "Recharger la page"
+          lienPanierVide.setAttribute('href',
+            'panier.html')
+        });
+    }
+
   }
+
 
 
 
@@ -148,14 +153,21 @@ window.onload = getProduitID();
 
 // récupération élément produit 
 let products = []
-for (let i = 0; i < panierJson.length; i++) {
 
-  // Création tableau products
-  products.push(panierJson[i].produit)
+if (panierJson) {
+
+  for (let i = 0; i < panierJson.length; i++) {
+
+    // Création tableau products
+    products.push(panierJson[i].produit)
 
 
+
+  }
 
 }
+
+
 
 // récupération élement formulaire
 
